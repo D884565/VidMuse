61a663c42eaf10b0f03a8c66f94302f2deafe46b
chenge <3011825955@qq.com>
2026-05-28 16:39:45 +0800

    feat: implement result merger and reranker modules

--- backend/v1/app/search/post_processing/__init__.py
+++ backend/v1/app/search/post_processing/__init__.py
@@ -1,9 +1,13 @@
 from .base import BasePostProcessorImpl
 from .deduplicator import Deduplicator
 from .filter import Filter
+from .merger import Merger
+from .reranker import Reranker
 
 __all__ = [
     "BasePostProcessorImpl",
     "Deduplicator",
-    "Filter"
+    "Filter",
+    "Merger",
+    "Reranker"
 ]
