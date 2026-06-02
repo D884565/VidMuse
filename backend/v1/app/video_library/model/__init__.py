0cfdce628a3dd3bddd9aa73ecd73f1eb124a7d6d
chenge <3011825955@qq.com>
2026-06-03 00:12:44 +0800

    feat: initialize video_library module structure

---
+++ backend/v1/app/video_library/model/__init__.py
@@ -0,0 +1,3 @@
+from backend.v1.app.video_library.model.video_library import VideoLibrary
+
+__all__ = ["VideoLibrary"]
---
+++ backend/v1/app/video_library/service/__init__.py
@@ -0,0 +1,3 @@
+from backend.v1.app.video_library.service.video_library_service import VideoLibraryService
+
+__all__ = ["VideoLibraryService"]
